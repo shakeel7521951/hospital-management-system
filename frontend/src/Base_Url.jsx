b7b8c1a018bd3c7209d098072fcb5f8ff150726e
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-06-16 00:01:48 +0500

    base url

--- frontend/src/Base_Url.jsx
+++ frontend/src/Base_Url.jsx
@@ -1,3 +1,3 @@
-// const BACKEND_URL = "https://hospital-mangement-backend-oh5g.onrender.com/api/v1";
-const BACKEND_URL = "http://localhost:4000/api/v1"
+const BACKEND_URL = "https://hospital-mangement-backend-oh5g.onrender.com/api/v1";
+// const BACKEND_URL = "http://localhost:4000/api/v1"
 export default BACKEND_URL;
